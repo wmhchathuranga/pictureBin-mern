216df25b63fa5c4d8023b1b132d28a6bf32d6166
Harshana Chathuranga <91200650+wmhchathuranga@users.noreply.github.com>
2023-04-05 18:08:53 +0530

    test page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <p className="underline">
+        Testing css
+      </p>
+      <div className="col text-center p-6 border">
+        <button className="border rounded-md border-black text-black bg-white p-4 shadow-[1px_1px_10px_3px_rgba(0,0,0,0.9)]">Submit me</button>
+      </div>
     </div>
   );
 }
